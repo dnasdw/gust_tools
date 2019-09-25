a6cd25d4ca0718ace99eef95c0087f8b0cf066f2
VitaSmith <vitasmith@rpc1.org>
2019-09-25 22:05:37 +0100

    add Linux support

--- A18_Decrypt.c
+++ A18_Decrypt.c
@@ -19,10 +19,26 @@
 
 #include <stdbool.h>
 #include <stdint.h>
+#include <inttypes.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#if defined(_WIN32)
 #include <windows.h>
+#else
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#endif
+
+#if defined(_WIN32)
+#define ftell64 _ftelli64
+#define fseek64 _fseeki64
+#else
+#define ftell64 ftello64
+#define fseek64 fseeko64
+#endif
 
 typedef struct {
     uint32_t unknown1;
@@ -44,6 +60,7 @@ static pak_entry64* entries = NULL;
 
 uint8_t blank_key[20] = { 0 };
 
+#if defined(_WIN32)
 bool create_path(char* path)
 {
     bool result = true;
@@ -75,12 +92,51 @@ bool create_path(char* path)
     } else if (!(attr & FILE_ATTRIBUTE_DIRECTORY)) {
         // Object already exists, but is not a dir
         SetLastError(ERROR_FILE_EXISTS);
+        result = false;
     }
 
     return result;
 }
+#else
+bool create_path(char* path)
+{
+    bool result = true;
+    struct stat st;
+    if (stat(path, &st) != 0) {
+        // Directory doesn't exist, create it
+        size_t pos = 0;
+        for (size_t n = strlen(path); n > 0; n--) {
+            if (path[n] == '/') {
+                pos = n;
+                break;
+            }
+        }
+        if (pos > 0) {
+            // Create parent dirs
+            path[pos] = 0;
+            char* new_path = (char*)malloc(sizeof(char) * (strlen(path) + 1));
+            if (new_path == NULL) {
+                fprintf(stderr, "ERROR: Can't allocate path\n");
+                return false;
+            }
+            strcpy(new_path, path);
+            result = create_path(new_path);
+            free(new_path);
+            path[pos] = '/';
+        }
+        // Create node:
+        result = result && (mkdir(path, 0755) == 0);
+    } else if (!S_ISDIR(st.st_mode)) {
+        // Object already exists, but is not a dir
+        errno = ENOTDIR;
+        result = false;
+    }
 
-void __inline decode(uint8_t* a, uint8_t* k, uint32_t length)
+    return result;
+}
+#endif
+
+__inline void decode(uint8_t* a, uint8_t* k, uint32_t length)
 {
     for (uint32_t i = 0; i < length; i++)
         a[i] ^= k[i % 20];
@@ -88,13 +144,17 @@ void __inline decode(uint8_t* a, uint8_t* k, uint32_t length)
 
 int main(int argc, char** argv)
 {
-    char app_name[64];
     if (argc != 2) {
+#if defined(_WIN32)
+        char app_name[64];
         _splitpath_s(argv[0], NULL, 0, NULL, 0, app_name, sizeof(app_name), NULL, 0);
-        printf("A18_decrypt (c) 2018-2019 Yuri Hime & VitaSmith\n\nUsage: %s <Gust PAK file>\n\n"
+#else
+        char* app_name = basename(argv[0]);
+#endif
+        printf("%s (c) 2018-2019 Yuri Hime & VitaSmith\n\nUsage: %s <Gust PAK file>\n\n"
             "Dumps the Gust PAK format archive to the current directory.\n"
             "If unpacked to the game directory, you can remove the .pak file\n"
-            "and it will use the unpacked assets. Have fun, modders!\n", app_name);
+            "and it will use the unpacked assets. Have fun, modders!\n", app_name, app_name);
         return 0;
     }
     FILE* src = fopen(argv[1], "rb");
@@ -122,9 +182,9 @@ int main(int argc, char** argv)
     }
 
     fread(entries, sizeof(pak_entry64), header.nb_entries, src);
-    int64_t file_data_offset = _ftelli64(src);
+    int64_t file_data_offset = ftell64(src);
     puts("OFFSET    SIZE     NAME");
-    char path[MAX_PATH];
+    char path[256];
     uint8_t* buf;
     bool skip_decode;
     for (uint32_t i = 0; i < header.nb_entries; i++) {
@@ -134,10 +194,16 @@ int main(int argc, char** argv)
                 skip_decode = false;
         if (!skip_decode)
             decode((uint8_t*)entries[i].filename, entries[i].key, 128);
-        printf("%09I64x %08x %s\n", entries[i].data_offset + file_data_offset, entries[i].length, entries[i].filename);
+#if !defined(_WIN32)
+        for (size_t n = 0; n < strlen(entries[i].filename); n++) {
+            if (entries[i].filename[n] == '\\')
+                entries[i].filename[n] = '/';
+        }
+#endif
+        printf("%09" PRIx64 " %08x %s\n", entries[i].data_offset + file_data_offset, entries[i].length, entries[i].filename);
         strcpy(path, (char*)entries[i].filename + 1);
         for (size_t n = strlen(path); n > 0; n--) {
-            if (path[n] == '\\') {
+            if ((path[n] == '\\') || (path[n] == '/')) {
                 path[n] = 0;
                 break;
             }
@@ -152,7 +218,7 @@ int main(int argc, char** argv)
             fprintf(stderr, "Can't open file %s\n", entries[i].filename + 1);
             continue;
         }
-        _fseeki64(src, entries[i].data_offset + file_data_offset, SEEK_SET);
+        fseek64(src, entries[i].data_offset + file_data_offset, SEEK_SET);
         buf = (unsigned char*)malloc(sizeof(unsigned char) * entries[i].length);
         fread(buf, 1, entries[i].length, src);
         if (!skip_decode)
---
+++ Makefile
@@ -0,0 +1,30 @@
+ifeq ($(OS),Windows_NT)
+  EXE := .exe
+else
+  EXE :=
+endif
+
+BIN=A18_Decrypt${EXE}
+SRC=A18_Decrypt.c
+OBJ=${SRC:.c=.o}
+DEP=${SRC:.c=.d}
+
+CFLAGS=-std=c99 -pipe -fvisibility=hidden -Wall -Wextra -Werror -DNDEBUG -D_GNU_SOURCE -O2
+LDFLAGS=-s
+
+.PHONY: all clean
+
+all: ${BIN}
+
+clean:
+	@${RM} ${BIN} ${OBJ} ${DEP}
+
+${BIN}: ${OBJ}
+	@echo [L] $@
+	@${CC} ${LDFLAGS} -o $@ $^
+
+%.o: %.c
+	@echo [C] $<
+	@${CC} ${CFLAGS} -MMD -c -o $@ $<
+
+-include ${DEP}
