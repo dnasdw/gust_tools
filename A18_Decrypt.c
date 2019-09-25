7b10fba7cc87554e2b9270ebb77571f037f87389
VitaSmith <vitasmith@rpc1.org>
2019-09-25 20:37:23 +0100

    more code cleanup
    Use stdint, remove DECODE_PAK stuff, harmonize variable
    names, fix compilation warnings and add build script.

--- A18_Decrypt.c
+++ A18_Decrypt.c
@@ -1,7 +1,7 @@
 /*
-  A18_Decrypt - Gust (Koei/Tecmo) archive decrypter/unpacker for PC games
+  A18_Decrypt - Archive unpacker for Gust (Koei/Tecmo) PC games 
   Copyright © 2019 VitaSmith
-  Copyright © 2018 Yuri Hime, shizukachan
+  Copyright © 2018 Yuri Hime (shizukachan)
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
@@ -18,162 +18,150 @@
 */
 
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <windows.h>
 
-struct fileHeader
-{
-    unsigned int unknown1;
-    unsigned int fileEntries;
-    unsigned int unknown2;
-    unsigned int unknown3;
-} typedef fileHeader;
+typedef struct {
+    uint32_t unknown1;
+    uint32_t nb_entries;
+    uint32_t unknown2;
+    uint32_t unknown3;
+} pak_header;
 
-struct fileEntry    //len = 0xA8
-{
-    unsigned char filename[128];
-    unsigned int length;
-    unsigned char key[20];
-    unsigned __int64 data_offset;   //only change from A17
-    unsigned __int64 dummy;         //and an extra padding byte. Why, I don't know.
-} typedef fileEntry;
+typedef struct {
+    char     filename[128];
+    uint32_t length;
+    uint8_t  key[20];
+    uint64_t data_offset;
+    uint64_t dummy;
+} pak_entry64;
 
-static fileHeader Header;
-static fileEntry* Entries = NULL;
+static pak_header header;
+static pak_entry64* entries = NULL;
 
-bool CreatePath(char* wsPath)       //ripped off the web. don't judge me
+uint8_t blank_key[20] = { 0 };
+
+bool create_path(char* path)
 {
-    DWORD attr;
-    int pos;
     bool result = true;
-    // Look for existing object:
-    attr = GetFileAttributesA((LPCSTR)wsPath);
-    if (0xFFFFFFFF == attr)         // doesn't exist yet - create it!
-    {
-        pos = 0;
-        for (int n = strlen(wsPath); n > 0; n--)
-            if (wsPath[n] == '\\') {
+    DWORD attr = GetFileAttributesA(path);
+    if (attr == 0xFFFFFFFF) {
+        // Directory doesn't exist, create it
+        size_t pos = 0;
+        for (size_t n = strlen(path); n > 0; n--) {
+            if (path[n] == '\\') {
                 pos = n;
                 break;
             }
-        if (0 < pos) {
-            // Create parent dirs:
-            wsPath[pos] = 0;
-            char* newPath = (char*)malloc(sizeof(char) * (strlen(wsPath) + 1));
-            strcpy(newPath, wsPath);
-            result = CreatePath(newPath);
-            free(newPath);
-            wsPath[pos] = '\\';
+        }
+        if (pos > 0) {
+            // Create parent dirs
+            path[pos] = 0;
+            char* new_path = (char*)malloc(sizeof(char) * (strlen(path) + 1));
+            if (new_path == NULL) {
+                fprintf(stderr, "ERROR: Can't allocate path\n");
+                return false;
+            }
+            strcpy(new_path, path);
+            result = create_path(new_path);
+            free(new_path);
+            path[pos] = '\\';
         }
         // Create node:
-        result = result && CreateDirectoryA((LPCSTR)wsPath, NULL);
-    } else if (FILE_ATTRIBUTE_DIRECTORY != attr) {  // object already exists, but is not a dir
+        result = result && CreateDirectoryA((LPCSTR)path, NULL);
+    } else if (FILE_ATTRIBUTE_DIRECTORY != attr) {
+        // Object already exists, but is not a dir
         SetLastError(ERROR_FILE_EXISTS);
-        result = false;
     }
 
     return result;
 }
 
-void decode(unsigned char* a, unsigned char* k, unsigned int length)
-{//a17 has a SSE2 optimized version. This one is done bytewise because I'm lazy.
-    for (int i = 0; i < length; i++)    // Dear Japan,
-        a[i] = a[i] ^ k[i % 20];        // It would be nice if you actually used some kind of fun encryption instead of this silly crap.
+void __inline decode(uint8_t* a, uint8_t* k, uint32_t length)
+{
+    for (uint32_t i = 0; i < length; i++)
+        a[i] ^= k[i % 20];
 }
-
-unsigned char blank_key[20] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
 int main(int argc, char** argv)
 {
+    char app_name[64];
     if (argc != 2) {
-        printf("A18_decrypt by Yuri Hime\nUsage: %s <Atelier PAK file>\n\nDumps the Atelier PAK format archive to the current directory.\nIf unpacked to the Atelier Sophie directory, you can remove the PACK00.pak file\nand it will use the unpacked assets. Have fun, modders!", argv[0]);
+        _splitpath_s(argv[0], NULL, 0, NULL, 0, app_name, sizeof(app_name), NULL, 0);
+        printf("A18_decrypt (c) 2018-2019 Yuri Hime & VitaSmith\n\nUsage: %s <Gust PAK file>\n\n"
+            "Dumps the Gust PAK format archive to the current directory.\n"
+            "If unpacked to the game directory, you can remove the .pak file\n"
+            "and it will use the unpacked assets. Have fun, modders!\n", app_name);
         return 0;
     }
-    FILE* A = fopen(argv[1], "rb");
-    if (A == NULL) {
+    FILE* src = fopen(argv[1], "rb");
+    if (src == NULL) {
         fprintf(stderr, "Can't open PAK file");
         return -1;
     }
-    //#define DECODE_PAK 1
-#ifdef DECODE_PAK
-    //to use this feature, copy the encrypted PACK00.pak file to ./PACK00.dec
-    //It will re-create the PACK00.pak file, except all data will be unencrypted, into ./PACK00.dec
-    //The game engine should accept ./PACK00.dec as equivalent to PACK00.pak.
-    FILE* C = fopen("PACK01.dec", "r+b");
-    //used to see if the engine cares about unencrypted data... it doesn't.
-#endif
-    if (fread(&Header, sizeof(Header), 1, A) == 0) {
-        fprintf(stderr, "can't read header");
+ 
+    if (fread(&header, sizeof(header), 1, src) == 0) {
+        fprintf(stderr, "Can't read header");
         return -1;
     }
-#ifdef DECODE_PAK
-    fwrite(&Header, sizeof(Header), 1, C);
-#endif
-    if ((Header.unknown1 != 0x20000) || (Header.unknown2 != 0x10) || (Header.unknown3 != 0x0D)) {
-        //		fprintf(stderr,"WARNING: signature of file doesn't match Atelier Sophie archive.\n");
-        fprintf(stderr, "WARNING: signature of file doesn't match Atelier Firis archive.\n");
+
+    if ((header.unknown1 != 0x20000) || (header.unknown2 != 0x10) || (header.unknown3 != 0x0D)) {
+        fprintf(stderr, "WARNING: Signature doesn't match expected PAK file format.\n");
     }
-    if (Header.fileEntries > 16384) {
-        fprintf(stderr, "WARNING: fileEntries over 16384, is this a supported archive?\n");
+
+    if (header.nb_entries > 16384) {
+        fprintf(stderr, "WARNING: More than 16384 entries, is this a supported archive?\n");
     }
-    Entries = (fileEntry*)malloc(sizeof(fileEntry) * Header.fileEntries);
-    fread(Entries, sizeof(fileEntry), Header.fileEntries, A);
-    __int64 file_data_offset = _ftelli64(A);
+    entries = (pak_entry64*)malloc(sizeof(pak_entry64) * header.nb_entries);
+    if (entries == NULL) {
+        fprintf(stderr, "ERROR: Can't allocate entries\n");
+        return -1;
+    }
+
+    fread(entries, sizeof(pak_entry64), header.nb_entries, src);
+    int64_t file_data_offset = _ftelli64(src);
     puts("OFFSET    SIZE     NAME");
-    char path[256];
-    unsigned char* buf;
-    unsigned char skip_decode;
-    for (int i = 0; i < Header.fileEntries; i++) {
-        skip_decode = 1;
+    char path[MAX_PATH];
+    uint8_t* buf;
+    bool skip_decode;
+    for (uint32_t i = 0; i < header.nb_entries; i++) {
+        skip_decode = true;
         for (int j = 0; j < 20; j++)
-            if (Entries[i].key[j] != blank_key[j])
-                skip_decode = 0;
+            if (entries[i].key[j] != blank_key[j])
+                skip_decode = false;
         if (!skip_decode)
-            decode(Entries[i].filename, Entries[i].key, 128);
-        printf("%09I64x %08x %s\n", Entries[i].data_offset + file_data_offset, Entries[i].length, Entries[i].filename);
-        strcpy(path, (char*)Entries[i].filename + 1);
-        for (int n = strlen(path); n > 0; n--) {
+            decode((uint8_t*)entries[i].filename, entries[i].key, 128);
+        printf("%09I64x %08x %s\n", entries[i].data_offset + file_data_offset, entries[i].length, entries[i].filename);
+        strcpy(path, (char*)entries[i].filename + 1);
+        for (size_t n = strlen(path); n > 0; n--) {
             if (path[n] == '\\') {
                 path[n] = 0;
                 break;
             }
         }
-        if (CreatePath(path) == false) {
+        if (create_path(path) == false) {
             fprintf(stderr, "Can't create path %s\n", path);
             continue;
         }
-        FILE* B = NULL;
-        B = fopen((char*)(Entries[i].filename + 1), "wb");
-        if (B == NULL) {
-            fprintf(stderr, "Can't open file %s\n", Entries[i].filename + 1);
+        FILE* dst = NULL;
+        dst = fopen((char*)(entries[i].filename + 1), "wb");
+        if (dst == NULL) {
+            fprintf(stderr, "Can't open file %s\n", entries[i].filename + 1);
             continue;
         }
-        _fseeki64(A, Entries[i].data_offset + file_data_offset, SEEK_SET);
-#ifdef DECODE_PAK
-        _fseeki64(C, Entries[i].data_offset + file_data_offset, SEEK_SET);
-#endif
-        buf = (unsigned char*)malloc(sizeof(unsigned char) * Entries[i].length);
-        fread(buf, 1, Entries[i].length, A);
+        _fseeki64(src, entries[i].data_offset + file_data_offset, SEEK_SET);
+        buf = (unsigned char*)malloc(sizeof(unsigned char) * entries[i].length);
+        fread(buf, 1, entries[i].length, src);
         if (!skip_decode)
-            decode(buf, Entries[i].key, Entries[i].length);
-        fwrite(buf, 1, Entries[i].length, B);
-#ifdef DECODE_PAK
-        fwrite(buf, 1, Entries[i].length, C);
-#endif
+            decode(buf, entries[i].key, entries[i].length);
+        fwrite(buf, 1, entries[i].length, dst);
         free(buf);
-        fclose(B);
+        fclose(dst);
     }
-#ifdef DECODE_PAK
-    _fseeki64(C, 0x10, SEEK_SET);
-    for (int i = 0; i < Header.fileEntries; i++) {
-        memset(Entries[i].key, 0, sizeof(Entries[i].key));
-        fwrite(&Entries[i], sizeof(fileEntry), 1, C);
-    }
-    fclose(C);
-#endif
-    fclose(A);
-    free(Entries);
+    fclose(src);
+    free(entries);
     return 0;
 }
---
+++ build.cmd
@@ -0,0 +1,15 @@
+@echo off
+set APP_NAME=A18_Decrypt.exe
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat" -arch=amd64 -host_arch=amd64
+cd /d "%~dp0"
+
+set CL=/nologo /errorReport:none /Gm- /GF /GS- /MP /MT /W4 /WX /wd4324 /wd4996 /D_CRT_SECURE_NO_DEPRECATE
+set LINK=/errorReport:none /INCREMENTAL:NO
+
+set CL=%CL% /Ox
+rem set CL=%CL% /Od /Zi
+rem set LINK=%LINK% /DEBUG
+
+cl.exe A18_Decrypt.c /Fe%APP_NAME%
+if %ERRORLEVEL% equ 0 echo =^> %APP_NAME%
+pause
