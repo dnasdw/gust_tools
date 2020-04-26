0571e290cdb851f30c25c7f669211e489362fcaa
VitaSmith <vitasmith@rpc1.org>
2020-04-27 00:18:43 +0100

    gust_ebm: add support for Nelke .ebm files
    * Closes #11

--- gust_ebm.c
+++ gust_ebm.c
@@ -26,27 +26,26 @@
 #include "util.h"
 #include "parson.h"
 
-#pragma pack(push, 1)
-typedef struct {
+/* An ebm structure is as follows:
     uint32_t type;          // always seems to be set to 2
     uint32_t voice_id;      // id of the voice for the speaking character
     uint32_t unknown1;      // ???
     uint32_t name_id;       // id of the name to use for the speaking character
     uint32_t extra_id;      // seems to be -1 for system messages
     uint32_t expr_id;       // serious = 0x09, surprise = 0x0a, happy = 0x0c, etc.
+    uint32_t unknown3;      // [OPTIONAL] Used by Nelke but set to 0xffffffff
+    uint32_t unknown4;      // [OPTIONAL] Used by Nelke but set to 0xffffffff
     uint32_t msg_id;        // sequential id of the message
     uint32_t unknown2;      // ???
     uint32_t msg_length;    // length of msg_string
     char     msg_string[0]; // text message to display
-    // Note: you can use "<CR>" within a message to force a line break as well as
-    // display small images (e.g. controller buttons) with things like "<IM10>", "<IM00>"
-} ebm_message;
-#pragma pack(pop)
+ */
 
 int main_utf8(int argc, char** argv)
 {
     int r = -1;
     uint8_t* buf = NULL;
+    char* ebm_message;
     FILE* file = NULL;
     JSON_Value* json = NULL;
 
@@ -72,6 +71,9 @@ int main_utf8(int argc, char** argv)
             fprintf(stderr, "ERROR: Cannot create file '%s'\n", filename);
             goto out;
         }
+        uint32_t header_size = (uint32_t)json_object_get_number(json_object(json), "header_size");
+        if (header_size == 0)
+            header_size = 9;
         uint32_t nb_messages = (uint32_t)json_object_get_number(json_object(json), "nb_messages");
         if (fwrite(&nb_messages, sizeof(uint32_t), 1, file) != 1) {
             fprintf(stderr, "ERROR: Can't write number of messages\n");
@@ -82,25 +84,31 @@ int main_utf8(int argc, char** argv)
             fprintf(stderr, "ERROR: Number of messages doesn't match the array size\n");
             goto out;
         }
+        uint32_t ebm_header[11];
         for (uint32_t i = 0; i < nb_messages; i++) {
             JSON_Object* json_message = json_array_get_object(json_messages, i);
-            ebm_message message = { 0 };
-            message.type = (uint32_t)json_object_get_number(json_message, "type");
-            message.voice_id = (uint32_t)json_object_get_number(json_message, "voice_id");
-            message.unknown1 = (uint32_t)json_object_get_number(json_message, "unknown1");
-            message.name_id = (uint32_t)json_object_get_number(json_message, "name_id");
-            message.extra_id = (uint32_t)json_object_get_number(json_message, "extra_id");
-            message.expr_id = (uint32_t)json_object_get_number(json_message, "expr_id");
-            message.msg_id = (uint32_t)json_object_get_number(json_message, "msg_id");
-            message.unknown2 = (uint32_t)json_object_get_number(json_message, "unknown2");
+            memset(ebm_header, 0, sizeof(ebm_header));
+            uint32_t j = 0;
+            ebm_header[j] = (uint32_t)json_object_get_number(json_message, "type");
+            ebm_header[++j] = (uint32_t)json_object_get_number(json_message, "voice_id");
+            ebm_header[++j] = (uint32_t)json_object_get_number(json_message, "unknown1");
+            ebm_header[++j] = (uint32_t)json_object_get_number(json_message, "name_id");
+            ebm_header[++j] = (uint32_t)json_object_get_number(json_message, "extra_id");
+            ebm_header[++j] = (uint32_t)json_object_get_number(json_message, "expr_id");
+            if (header_size == 11) {
+                ebm_header[++j] = 0xffffffff;
+                ebm_header[++j] = 0xffffffff;
+            }
+            ebm_header[++j] = (uint32_t)json_object_get_number(json_message, "msg_id");
+            ebm_header[++j] = (uint32_t)json_object_get_number(json_message, "unknown4");
             const char* msg_string = json_object_get_string(json_message, "msg_string");
-            message.msg_length = (uint32_t)strlen(msg_string) + 1;
-            if (fwrite(&message, sizeof(ebm_message), 1, file) != 1) {
-                fprintf(stderr, "ERROR: Can't write message entry\n");
+            ebm_header[++j] = (uint32_t)strlen(msg_string) + 1;
+            if (fwrite(ebm_header, header_size * sizeof(uint32_t), 1, file) != 1) {
+                fprintf(stderr, "ERROR: Can't write message header\n");
                 goto out;
             }
-            if (fwrite(msg_string, 1, message.msg_length, file) != message.msg_length) {
-                fprintf(stderr, "ERROR: Can't write message entry\n");
+            if (fwrite(msg_string, 1, ebm_header[j], file) != ebm_header[j]) {
+                fprintf(stderr, "ERROR: Can't write message data\n");
                 goto out;
             }
         }
@@ -112,37 +120,61 @@ int main_utf8(int argc, char** argv)
             goto out;
         uint32_t nb_messages = getle32(buf);
         if (buf_size < sizeof(uint32_t) + nb_messages * sizeof(ebm_message)) {
-            fprintf(stderr, "ERROR: Invalid number of entries");
+            fprintf(stderr, "ERROR: Invalid number of entries\n");
             goto out;
         }
 
-        // Store the data we'll need to reconstruct the archibe to a JSON file
+        // Store the data we'll need to reconstruct the archive to a JSON file
         json = json_value_init_object();
         json_object_set_string(json_object(json), "name", basename(argv[argc - 1]));
         json_object_set_number(json_object(json), "nb_messages", nb_messages);
         JSON_Value* json_messages = json_value_init_array();
-        ebm_message* entry = (ebm_message*) &buf[sizeof(uint32_t)];
-        assert(sizeof(ebm_message) == 36);
+        uint32_t* ebm_header = (uint32_t*)&buf[sizeof(uint32_t)];
+        uint32_t header_size = 0;
         for (uint32_t i = 0; i < nb_messages; i++) {
+            uint32_t j = 0;
             JSON_Value* json_message = json_value_init_object();
-            uint32_t str_length = entry->msg_length;
-            json_object_set_number(json_object(json_message), "type", (double)entry->type);
-            json_object_set_number(json_object(json_message), "voice_id", (double)entry->voice_id);
-            if (entry->unknown1 != 0)
-                json_object_set_number(json_object(json_message), "unknown1", (double)entry->unknown1);
-            json_object_set_number(json_object(json_message), "name_id", (double)entry->name_id);
-            if (entry->extra_id != 0)
-                json_object_set_number(json_object(json_message), "extra_id", (double)entry->extra_id);
-            json_object_set_number(json_object(json_message), "expr_id", (double)entry->expr_id);
-            json_object_set_number(json_object(json_message), "msg_id", (double)entry->msg_id);
-            if (entry->unknown2 != 0)
-                json_object_set_number(json_object(json_message), "unknown2", (double)entry->unknown2);
+            json_object_set_number(json_object(json_message), "type", (double)ebm_header[j]);
+            if (ebm_header[j] > 0x10)
+                fprintf(stderr, "WARNING: Unexpected header type %d\n", ebm_header[j]);
+            json_object_set_number(json_object(json_message), "voice_id", (double)ebm_header[++j]);
+            if (ebm_header[++j] != 0)
+                json_object_set_number(json_object(json_message), "unknown1", (double)ebm_header[j]);
+            json_object_set_number(json_object(json_message), "name_id", (double)ebm_header[++j]);
+            if (ebm_header[++j] != 0)
+                json_object_set_number(json_object(json_message), "extra_id", (double)ebm_header[j]);
+            json_object_set_number(json_object(json_message), "expr_id", (double)ebm_header[++j]);
+            if (ebm_header[++j] > 0x00010000) {
+                if ((ebm_header[j++] != 0xffffffff) || (ebm_header[j++] != 0xffffffff)) {
+                    fprintf(stderr, "ERROR: Don't know how to handle this type of .ebm file\n");
+                    goto out;
+                }
+                if (header_size == 0) {
+                    header_size = 11;
+                } else if (header_size != 11) {
+                    fprintf(stderr, "ERROR: Unexpected header size (Got %d, expected 11)\n", header_size);
+                    goto out;
+                }
+            } else {
+                if (header_size == 0) {
+                    header_size = 9;
+                } else if (header_size != 9) {
+                    fprintf(stderr, "ERROR: Unexpected header size (Got %d, expected 9)\n", header_size);
+                    goto out;
+                }
+            }
+            json_object_set_number(json_object(json_message), "msg_id", (double)ebm_header[j]);
+            if (ebm_header[++j] != 0)
+                json_object_set_number(json_object(json_message), "unknown2", (double)ebm_header[j]);
             // Don't store msg_length since we'll reconstruct it
-            json_object_set_string(json_object(json_message), "msg_string", entry->msg_string);
+            uint32_t str_length = ebm_header[++j];
+            assert(str_length < 0x10000);
+            char* ptr = (char*)&ebm_header[header_size];
+            json_object_set_string(json_object(json_message), "msg_string", ptr);
             json_array_append_value(json_array(json_messages), json_message);
-            entry = &entry[1];
-            entry = (ebm_message*) &((uint8_t*)entry)[str_length];
+            ebm_header = (uint32_t*) &ptr[str_length];
         }
+        json_object_set_number(json_object(json), "header_size", header_size);
         json_object_set_value(json_object(json), "messages", json_messages);
         json_serialize_to_file_pretty(json, change_extension(argv[argc - 1], ".json"));
         r = 0;
